0676c5d27ea1d0f9b70cd05181f1743ef31b2f24
Kim Gunnarsson <kim.gunnarsson@gmail.com>
2017-12-25 21:11:46 +0100

    Add scoped service worker

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ if('serviceWorker' in navigator) {
 				console.log('ServiceWorker registration failed: ', err);
 			});
 
+			navigator.serviceWorker
+			.register('service-worker.scoped.js', {scope: 'scoped-test/'})
+			.then(function(registration) {
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			}, function(err) {
+				console.log('ServiceWorker registration failed: ', err);
+			});
+
 	});
 }
---
+++ service-worker.scoped.js
@@ -0,0 +1,40 @@
+var CACHE_NAME = 'demo-site-scoped-cache-v1';
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+		.open(CACHE_NAME)
+		.then(function(cache){
+			console.log('Open cache');
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response) {
+				if(response) {
+					return response;
+				}
+
+				var fetchRequest = event.request.clone();
+				return fetch(fetchRequest).then(
+					function(response) {
+						if(!response || response.status !== 200 || response.type !== 'basic') {
+							return response;
+						}
+
+						var responseForCache = response.clone();
+						caches.open(CACHE_NAME)
+						.then(function(cache) {
+							cache.put(event.request, responseForCache);
+						});
+
+						return response;
+					}
+				);
+			})
+	);
+});
