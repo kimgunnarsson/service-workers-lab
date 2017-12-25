c35718f35ab753f0573715a7302588cba9036e9e
Kim Gunnarsson <kim.gunnarsson@gmail.com>
2017-12-25 20:07:11 +0100

    Resolve path to service worker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator) {
 	window.addEventListener('load', function() {
 		navigator.serviceWorker
-			.register('/service-worker.js')
+			.register('./service-worker.js')
 			.then(function(registration) {
 				console.log('ServiceWorker registration successful with scope: ', registration.scope);
 			}, function(err) {
